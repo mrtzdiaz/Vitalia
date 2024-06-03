532cdee793f658369ba8405fad1219306028afc3
Daniel <mrtzdiazdaniel@gmail.com>
2024-06-03 13:18:45 -0600

    Se agrega la función para enviar el correo

--- nosotros/js/nosotros.js
+++ nosotros/js/nosotros.js
@@ -1,48 +1,59 @@
 // VARIABLES
-let nombre = document.getElementById("inputNombre");
-let email = document.getElementById("inputEmail");
-let telefono = document.getElementById("inputNumber");
-let mensaje = document.getElementById("inputMensaje");
+let nombre = document.getElementById("from_name");
+let email = document.getElementById("user_email");
+let telefono = document.getElementById("user_cel");
+let mensaje = document.getElementById("message");
 let alerta = document.getElementById("alertValidaciones");
 let isValid;
 let btn = document.getElementById("btn");
+let form = document.getElementById("form");
+const btnCorreo = document.getElementById('button');
+
 
 // VALIDACIONES NOMBRE
-function validacionNombre () {
+function validacionNombre() {
     if (nombre.value.length < 3) {
         alerta.innerHTML += `El <strong>Nombre</strong> no es correcto.<br>`;
         alerta.style.display = "block";
         nombre.style.border = "solid red medium";
         isValid = false;
-    }; // if length 
+        return false
+    }
+    return true
 };
 // VALIDACIONES EMAIL
-function validacionEmail () {
- if(!email.value.includes("@")){
-    alerta.innerHTML += `El <strong>Correo Electrónico</strong> no es correcto.<br>`;
-    alerta.style.display = "block";
-    email.style.border = "solid red medium";
-    isValid = false;
- }
+function validacionEmail() {
+    if (!email.value.includes("@")) {
+        alerta.innerHTML += `El <strong>Correo Electrónico</strong> no es correcto.<br>`;
+        alerta.style.display = "block";
+        email.style.border = "solid red medium";
+        isValid = false;
+        return false
+    }
+    return true
 };
 
 // VALIDACIONES TELEFONO
-function validacionTelefono () {
+function validacionTelefono() {
     if (telefono.value.length != 10 || isNaN(telefono.value) || telefono.value.includes(".")) {
         alerta.innerHTML += `El <strong>Número telefónico</strong> no es correcto.<br>`;
         alerta.style.display = "block";
         telefono.style.border = "solid red medium";
         isValid = false;
+        return false
     }
+    return true
 }
 // VALIDACIONES CARACTERES MENSAJE
-function validacionMensaje () {
+function validacionMensaje() {
     if (mensaje.value.length >= 200) {
         alerta.innerHTML += `El <strong>Mensaje</strong> no debe exceder 200 caracteres.<br>`;
         alerta.style.display = "block";
         mensaje.style.border = "solid red medium";
         isValid = false;
-    }; // if length 
+        return false
+    }
+    return true
 };
 
 // FUNCION BUTON
@@ -56,8 +67,37 @@ btn.addEventListener("click", function (event) {
     mensaje.style.border = "";
     isValid = true;
 
-    validacionNombre();
-    validacionEmail();
-    validacionTelefono();
-    validacionMensaje();
+    if(!validacionNombre()){
+        isValid = false;
+    }
+    if(!validacionEmail()){
+        isValid = false;
+    }
+    if(!validacionTelefono()){
+        isValid = false;
+    }
+    if(!validacionMensaje()){
+        isValid = false;
+    }
+
+    if(isValid){
+        enviarCorreo();
+    }
 });
+
+
+// Función para mandar el correo
+function enviarCorreo() {
+    btn.value = 'Enviando...';
+    const serviceID = 'default_service';
+    const templateID = 'template_ynzt6oh';
+
+    emailjs.sendForm(serviceID, templateID, form)
+        .then(() => {
+            btn.value = 'Enviado!';
+            alert('Enviado!');
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        });
+};

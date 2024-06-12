ee0e50c1502f68ad04b130906e3f6fce8392fbd3
Diego <diego@Diegos-MacBook-Pro.local>
2024-06-11 22:46:29 -0600

    se agregan productos al slide en productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,5 +1,7 @@
 let mainContainer = document.getElementById("mainProducts");
 let carrusel1 = document.getElementById("carruselUno");
+let carrusel2 = document.getElementById("carruselDos");
+let carrusel3 = document.getElementById("carruselTres");
 
 
 let productoPrincipales = [
@@ -14,9 +16,14 @@ let productosVarios = [
     {'name':"Jarabe Broncolin Etiqueta Azul Mezcla de Plantas (250 ml)", 'img':"jarabe azul.webp", 'description':"Para el alivio de los sintomas del resfriado comun y tos.", 'price':"$214.00"},
     {'name':"Jarabe Eucalín Mezla de plantas y propóleo (240 ml)", 'img':"jarabe.webp", 'description':"Auxiliar en el tratamiento de Tos, Flemas, Resfriado Común o Tos de Fumador.", 'price':"$100.00"},
     {'name':"Shampoo Anticaída Romero y Sábila Árbol Verde (500 ml)", 'img':"shampoo.webp", 'description':"Shampoo natural Anticaída combate la caída y estimula el crecimiento de tu cabello con ingredientes naturales como la Sábila, espinosillo, ortiguilla, romero, coparche y cáscara de nogal; promoviendo un cabello sano y fuerte.", 'price':"$410.00"},
+    {'name':"Eucalipto jarabe (140 ml)", 'img':"Eucalipto.jpg", 'description':"Eucaliptine funciona como un auxiliar en el tratamiento de la tos seca y tos con flemas, así como en el alivio de la garganta irritada", 'price':"$410.00"},
+    {'name':"Fibra vegetal (450 gr)", 'img':"fibravegetal.jpeg", 'description':"Proteína vegetal en polvo sabor chocolate a base de chícharo y arroz, adicionado con enzimas y probióticos que ayudan a una buena salud digestiva", 'price':"$300.00"},
+    {'name':"Chia mia té negro (350 gr)", 'img':"Chiatenegro.png", 'description':"Un té real para amantes de lo natural. Infusionado con hojas de té real, sin azúcares añadidos ni saborizantes y 100% natural.", 'price':"$80.00"},
     {'name':"Té Verde en polvo (300 gr)", 'img':"te-verde.jpg", 'description':"#", 'price':"$220.00"},
     {'name':"Jarabe Eucalín Infantil Mezla de plantas y propóleo (240 ml)", 'img':"jarabeniño.webp", 'description':"Auxiliar en el tratamiento de Tos y Flemas", 'price':"$80.00"}
-];
+    
+
+  ];
 
 function addItem(item, continer){
     item.forEach(element => {
@@ -44,4 +51,6 @@ function addItem(item, continer){
 }
 
 addItem(productoPrincipales, mainContainer);
-addItem(productosVarios, carrusel1);
+addItem(productosVarios.slice(0,3), carrusel1);
+addItem(productosVarios.slice(3,6), carrusel2);
+addItem(productosVarios.slice(6,9), carrusel3);

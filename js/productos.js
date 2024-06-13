c5132a73fbef165ee51dc6ce3baafc5ede6a2448
Daniel <mrtzdiazdaniel@gmail.com>
2024-06-13 17:14:37 -0600

    Se agregan los documentos del formulario para agregar productos

--- js/productos.js
+++ js/productos.js
@@ -1,3 +1,4 @@
+
 let mainContainer = document.getElementById("mainProducts");
 let carrusel1 = document.getElementById("carruselUno");
 let carrusel2 = document.getElementById("carruselDos");
@@ -23,41 +24,41 @@ let productosVarios = [
     {'name':"Jarabe Eucalín Infantil Mezla de plantas y propóleo (240 ml)", 'img':"jarabeniño.webp", 'description':"Auxiliar en el tratamiento de Tos y Flemas", 'price':"$80.00"}
   ];
 
- function addItem(item, continer){
-    item.forEach(element => {
-        continer.insertAdjacentHTML("beforeend",
-            `<div class="col">
-              <div class="card">
-              <div class="card-img">
-              <img src="/public/img/${element.img}" class="mx-auto d-block" alt="Aceites esenciales">
-              </div>
-                <div class="card-body">
-                <div class="card-title">
-                <h5 class="title">${element.name}</h5>
-                </div>
-                  <h5 id="rate">
-                  <strong>5.0</strong>
-                  <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
-                  <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
-                  <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
-                  <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
-                  <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
-                  </h5>
-                  <h3>${element.price}</h3>
-                  <button type="button" class="btn mx-auto d-block"><strong>¡Lo quiero!</button>
-                </div>
-              </div>
-            </div>`)
-        
-    });
-}
-
 navPage.forEach((item) => {
     item.addEventListener("click",function(){
         navPage.forEach((item) => {item.classList.remove("actived")});
         this.classList.add('actived');
     })
 })
+
+function addItem(item, container){
+  item.forEach(element => {
+      container.insertAdjacentHTML("beforeend",
+          `<div class="col">
+            <div class="card">
+            <div class="card-img">
+            <img src="/public/img/${element.img}" class="mx-auto d-block" alt="Aceites esenciales">
+            </div>
+              <div class="card-body">
+              <div class="card-title">
+              <h5 class="title">${element.name}</h5>
+              </div>
+                <h5 id="rate">
+                <strong>5.0</strong>
+                <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
+                <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
+                <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
+                <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
+                <i class="bi bi-star-fill" style="color:#E5C900; margin:0; font-size: 20px;"></i>
+                </h5>
+                <h3>${element.price}</h3>
+                <button type="button" class="btn mx-auto d-block"><strong>¡Lo quiero!</button>
+              </div>
+            </div>
+          </div>`)
+      
+  });
+}
 
 
 addItem(productoPrincipales, mainContainer);

aa8c070d69e9b89a0b36a4f3101a2d63f52721ab
veronica <veronica.bedolla.1991@gmail.com>
2024-07-10 11:28:52 -0500

    Se eresuelven conflictos

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,15 @@
 let header = document.getElementsByTagName('header');
-let usuario = JSON.parse(localStorage.getItem("usuario"));
 
 
+let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+let usuario = null
+let logeado = null
+if(usuarios != null){
+    usuario = usuarios.find(us => us.login);
+    logeado = (usuarios.find(us => us.login)==undefined?false:true);
+   
+}
+     
 let navbar = `<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #143027;">
     <div class="container-fluid">
 
@@ -52,13 +60,13 @@ let navbar = `<nav class="navbar navbar-expand-lg navbar-dark" style="background
             <button class="btn dropdown-toggle d-flex align-items-center" style="color: #fff; padding: 0; border: none; background: none;" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="bi bi-person-circle" style="color:#ccdbdc; font-size:40px;"></i>
                 <span class="text-white ms-2 me-3">
-                    ${usuario && usuario.login ? usuario.name.toUpperCase() : "Usuario"}
+                    ${logeado ? usuario.name.toUpperCase() : "Usuario"}
                 </span>
             </button>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                ${usuario && usuario.login ?
-        `<li><a class="dropdown-item" href="#" onclick="cerrarSesion()">Cerrar Sesión</a></li>` :
-        `<li><a class="dropdown-item" href="../html/iniciar_sesion.html">Iniciar Sesión</a></li>
+                ${logeado ?
+                    `<li><a class="dropdown-item" href="#" onclick="cerrarSesion()">Cerrar Sesión</a></li>` :
+                    `<li><a class="dropdown-item" href="../html/iniciar_sesion.html">Iniciar Sesión</a></li>
                     <li><a class="dropdown-item" href="../html/registro.html">Registro</a></li>`}
             </ul>
         </div>
@@ -86,10 +94,10 @@ navbarItems.forEach(function (item) {
     }
 });
 
-function cerrarSesion() {
-    usuario.login = false;
-    localStorage.setItem("usuario", JSON.stringify(usuario));
-    window.location.href = "http://127.0.0.1:3003/index.html";
+function cerrarSesion(){
+        usuario.login = false;
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+        window.location.href= "http://127.0.0.1:3003/index.html";
 }
 
 function insertNav() {
